c8a96d490e758fe01b549d91974470ac3163c7e7
Olamgir <ollomurodovolamgir233@gmail.com>
2023-11-09 22:40:41 +0900

    feat: checking react router dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,47 @@
 import React from "react";
 import "../css/App.css";
-import { Container } from "@mui/material";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Users from "./components/users";
+import Dishes from "./components/dishes";
 
 function App() {
-  return <Container></Container>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dishes">Dishes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+        renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/dishes">
+            <Dishes />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
+function Home() {
+  return <h2>Home</h2>;
+}
---
+++ src/app/components/dishes.tsx
@@ -0,0 +1,5 @@
+function Dishes() {
+  return <h2>Dishes</h2>;
+}
+
+export default Dishes;
