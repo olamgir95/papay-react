39b11d574e2124ef719cbff67f9d663d6ec6f4ad
Olamgir <ollomurodovolamgir233@gmail.com>
2023-11-13 00:10:25 +0900

    feat: navbar design finished

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../css/App.css";
 import "../css/navbar.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { RestaurantPage } from "./screens/RestaurantPage";
 import { OrdersPage } from "./screens/OrdersPage";
 import { CommunityPage } from "./screens/CommunityPage";
--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -1,11 +1,52 @@
-import { Container, Stack } from "@mui/material";
+import { Box, Container, Stack } from "@mui/material";
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function NavbarHome() {
   return (
     <div className="home_navbar">
       <Container>
-        <Stack flexDirection={"row"}></Stack>
+        <Stack
+          flexDirection={"row"}
+          className="navbar_config"
+          justifyContent={"space-between"}
+        >
+          <Box>
+            <img src="/icons/papayText.svg" alt="" />
+          </Box>
+          <Stack
+            flexDirection={"row"}
+            justifyContent={"space-evenly"}
+            alignItems={"center"}
+            className="navbar_links"
+          >
+            <Box className="hover-line">
+              <NavLink to="/" activeClassName="underline" exact>
+                Home
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <NavLink to="/restaurant" activeClassName="underline">
+                Restaurant
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <NavLink to="/orders" activeClassName="underline">
+                Orders
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <NavLink to="/community" activeClassName="underline">
+                Community
+              </NavLink>
+            </Box>
+            <Box className="hover-line">
+              <NavLink to="/help" activeClassName="underline">
+                Help
+              </NavLink>
+            </Box>
+          </Stack>
+        </Stack>
       </Container>
     </div>
   );
