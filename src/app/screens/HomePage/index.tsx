8cec3974a574004b07dc66fd527c15950c495078
Olamgir <ollomurodovolamgir233@gmail.com>
2023-12-21 15:42:02 +0900

    fix: usage of slice and selectors on homePgae screen component

--- src/app/screens/HomePage/index.tsx
+++ src/app/screens/HomePage/index.tsx
@@ -7,12 +7,34 @@ import BestRestaurants from "./bestRestaurants";
 import Events from "./events";
 import Recommendations from "./recomendations";
 import "../../../css/home.css";
+import { createSelector } from "reselect";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setTopRestaurants } from "./slice";
+import { Restaurant } from "../../../types/user";
+import { retrieveTopRestaurants } from "./selector";
+import { useDispatch, useSelector } from "react-redux";
 
+//redux slice
+const actionDispatch = (dispatch: Dispatch) => ({
+  setTopRestaurants: (data: Restaurant[]) => dispatch(setTopRestaurants(data)),
+});
+// redux slector
+const TopRestaurantsRetriever = createSelector(
+  retrieveTopRestaurants,
+  (topRestaurants) => ({
+    topRestaurants,
+  })
+);
 export function HomePage() {
+  const { setTopRestaurants } = actionDispatch(useDispatch());
+  const { topRestaurants } = useSelector(TopRestaurantsRetriever);
+  console.log("topres", topRestaurants);
+
   //selector: store => store
   useEffect(() => {
     // backend data request =>
 
+    setTopRestaurants([]);
     // slice data => store
     return;
   }, []);
