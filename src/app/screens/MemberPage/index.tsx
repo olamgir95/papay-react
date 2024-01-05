dddd8e6f45359a56251ba7cf98a10278ccbd8a02
Olamgir <ollomurodovolamgir233@gmail.com>
2024-01-05 19:01:25 +0900

    feat: organize retrieve of member page url query data

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,17 +1,30 @@
 import React from "react";
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useLocation, useRouteMatch } from "react-router-dom";
 import VisitOtherPage from "./VisitOtherPage";
 import VisitMyPage from "./VisitMyPage";
+
+function useQuery() {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 export function MemberPage(props: any) {
   const member = useRouteMatch();
   console.log(member);
+  const query = useQuery();
+  const chosen_mb_id: string | null = query.get("mb_id") ?? null;
+  const chosen_art_id: string | null = query.get("art_id") ?? null;
+  console.log("query", query);
 
   return (
     <div className="member_page">
       <Switch>
         <Route path={`${member.path}/other`}>
-          <VisitOtherPage verifyMemberData={props.verifedMemberData} />
+          <VisitOtherPage
+            verifyMemberData={props.verifedMemberData}
+            chosen_mb_id={chosen_mb_id}
+            chosen_art_id={chosen_art_id}
+          />
         </Route>
         <Route path={`${member.path}`}>
           <VisitMyPage verifyMemberData={props.verifedMemberData} />
